e9bb24800180ce2137aabcfde9cb72d456083f39
walid-1 <wilwil1515@hotmail.com>
2021-02-26 12:26:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import AdminScreen from "./screens/AdminScreen";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="grid-container">
+            <header>
+              <Link to="/">TuniStore</Link>
+              <Link to="/admin">Admin</Link>
+            </header>
+            <main>
+              <Route path="/admin" component={AdminScreen} />
+              <Route path="/" component={HomeScreen} exact />
+            </main>
+            <footer>Copyright © 2021-2022 Web E-commerce. All Rights Reserved | Privacy Policy. </footer>
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
+
+export default App;
